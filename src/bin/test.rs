63b2bfa8faa75be93c89d6bffaef09d92f4a8ace
root <root@DESKTOP-RHAANIS.localdomain>
2022-02-06 01:04:23 +0100

    updates

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -18,26 +18,51 @@ fn main() -> Result<(), CryptoAPIError> {
 
     // Input: (LWE Params: LWEParams, RLWE Params: RLWEParams, Max Threads: usize, Save: bool)
     // Initialize a TFHE instance
-    /*let tfhe = Tfheconcurrency::new(&LWE80_650, &RLWE80_1024_1, 2, false);
-
+    let mut tfhe = Tfheconcurrency::new(&LWE80_256, &RLWE80_1024_1, 2, false);
 
     // Input: (Val: f64, Len: usize)
     // Get a Vector of Ciphertexts with value Val and length Len
-    let c_vec = tfhe.get_ctxt_vec(0., 1024);
 
-    // Input: (Vector: Vec<LWE>, f: fn) 
-    // Evaluate the function f on the Vector "Vector"
-    let start = Instant::now();
-    let res_vec = tfhe.para_boot(c_vec.clone(), plus_1);
-    println!("{:.3}", (start.elapsed().as_micros() as f32)/ 1_000_000.);
-    println!("{}, {}", res_vec.len(), res_vec[0].decrypt_decode(&tfhe.sk1).unwrap());
-    */
-    let tfhe_pool = TfheconcurrencyPool::new(&LWE80_650, &RLWE80_1024_1, 2, false);
+    let mut lenghts = vec![];
+    for i in 1..32{
+        lenghts.push(i*10);
+    }
 
-    let start = Instant::now();
-    let res_vec = tfhe_pool.para_boot_pool(c_vec.clone(), plus_1);
-    println!("{:.3}", (start.elapsed().as_micros() as f32)/ 1_000_000.);
-    println!("{}, {}", res_vec.len(), res_vec[0].decrypt_decode(&tfhe.sk1).unwrap());
+    for nbr in lengths.iter(){
 
+        let c_vec = tfhe.get_ctxt_vec(0., nbr.clone());
+
+        // Input: (Vector: Vec<LWE>, f: fn) 
+        // Evaluate the function f on the Vector "Vector"
+        let mut times = vec![];
+        let threads = vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+        for thread in threads.iter(){ 
+            
+            tfhe.max_threads = *thread;
+
+            let start = Instant::now();
+            
+            for _ in 0..10{
+                let _ = tfhe.para_boot(c_vec.clone(), plus_1);
+            }
+            times.push((start.elapsed().as_micros() as f32)/ 1_000_000.);
+            //println!("{:.3}", (start.elapsed().as_micros() as f32)/ 1_000_000.);
+            //println!("{}, {}", res_vec.len(), res_vec[0].decrypt_decode(&tfhe.sk1).unwrap());
+        }
+
+        let mut iter = times.iter().enumerate();
+        let init = iter.next().ok_or("Need at least one input").unwrap();
+        let res = iter.try_fold(init, |acc, x| {
+            let cmp = x.1.partial_cmp(acc.1).unwrap();
+            let min = if let std::cmp::Ordering::Less = cmp {
+                x
+            } else {
+                acc
+            };
+            Some(min)
+        });
+        println!("{}, {:?}", nbr, res.unwrap());
+    }
     Ok(())
 }
